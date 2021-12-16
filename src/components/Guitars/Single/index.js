ebf07c0e04c6ad2bb7f1d543a9617312e8b4b97c
dulcefabiola <dulcefabiola@gmail.com>
2021-12-15 18:39:45 -0600

    style stores

--- src/components/Guitars/Single/index.js
+++ src/components/Guitars/Single/index.js
@@ -43,23 +43,6 @@ const Single = () => {
                   {singleGuitar.descripcion}
                 </p>
               </div>
-
-              <div class="mt-6 flex items-center">
-                <svg
-                  class="flex-shrink-0 w-5 h-5 text-green-500"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                    clip-rule="evenodd"
-                  />
-                </svg>
-                <p class="ml-2 text-sm text-gray-500">En existencia</p>
-              </div>
             </section>
           </div>
 
--- src/components/Stores/SingleStore/index.js
+++ src/components/Stores/SingleStore/index.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import StoreContext from "../../../context/Store/StoreContext";
 import { useParams } from "react-router-dom";
 const SingleStore = () => {
@@ -7,20 +7,31 @@ const SingleStore = () => {
 
   const params = useParams();
   const id = params.id;
+  useEffect(() => {
+    getStore(id);
+  }, []);
   return (
-    <div>
-      Pagina individual de store
-      <button
-        onClick={() => {
-          getStore(id);
-        }}
-      >
-        Obtener una tienda
-      </button>
-      <h3>Store</h3>
-      <h4>Descripcion:{singleStore.domicilio}</h4>
-      <h4>Precio: {singleStore.telefono}</h4>
-    </div>
+    <>
+      <div class="bg-white">
+        <div class="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:gap-x-8">
+          <div class="lg:max-w-lg lg:self-end">
+            <div class="mt-4">
+              <h1 class="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+                {singleStore.domicilio}
+              </h1>
+            </div>
+
+            <section aria-labelledby="information-heading" class="mt-4">
+              <div class="flex items-center">
+                <p class="text-lg text-gray-900 sm:text-xl">
+                  Tel: {singleStore.telefono}
+                </p>
+              </div>
+            </section>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
